f64034e4e577748bbc369cedbfe7c6285179f7e6
sddragon616 <ksdragon616@naver.com>
2017-11-09 12:25:23 +0900

    20171109 update

--- ObjectData002_SwordMan.py
+++ ObjectData002_SwordMan.py
@@ -2,13 +2,15 @@ from ObjectData001_BasePlayer import *
 import json
 
 SwordMan_data_file = open('UnitData\\SwordMan.txt', 'r')
-SwordMan_data = json.load(SwordMan_data_file)
+sword_user_data = json.load(SwordMan_data_file)
 SwordMan_data_file.close()
 
 
 class SwordMan(Player):
+    global sword_user_data
+
     def __init__(self, x, y):
-        super(SwordMan, self).__init__(SwordMan_data[SwordMan]['HP'], SwordMan_data[SwordMan]['MAX_HP'], SwordMan_data[SwordMan]['MP'], SwordMan_data[SwordMan]['MAX_MP'], SwordMan_data[SwordMan]['Stamina'], SwordMan_data[SwordMan]['MAX_Stamina'], SwordMan_data[SwordMan]['STR'], SwordMan_data[SwordMan]['DEF'], SwordMan_data[SwordMan]['MAG'], SwordMan_data[SwordMan]['MR'], SwordMan_data[SwordMan]['MOVE_SPEED'], SwordMan_data[SwordMan]['ATK_SPEED'])
+        super(SwordMan, self).__init__(sword_user_data[SwordMan]['HP'], sword_user_data[SwordMan]['MAX_HP'], sword_user_data[SwordMan]['MP'], sword_user_data[SwordMan]['MAX_MP'], sword_user_data[SwordMan]['Stamina'], sword_user_data[SwordMan]['MAX_Stamina'], sword_user_data[SwordMan]['STR'], sword_user_data[SwordMan]['DEF'], sword_user_data[SwordMan]['MAG'], sword_user_data[SwordMan]['MR'], sword_user_data[SwordMan]['MOVE_SPEED'], sword_user_data[SwordMan]['ATK_SPEED'])
         self.image = load_image("Resource_Image\\Player001_SwordMan.png")
         self.x = x
         self.y = y
