1726abf646961fedb8ac28e841c630359e047287
sddragon616 <ksdragon616@naver.com>
2018-10-10 09:57:51 +0900

    20181010 HomeworkBattle

--- Map/TileSet/Field.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Field" tilewidth="32" tileheight="32" tilecount="480" columns="30">
- <image source="Field.png" width="960" height="512"/>
-</tileset>
